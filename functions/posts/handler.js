2dee8223af71fa766d9c482f354c6a10531550b7
abalone0204 <abalone0204@gmail.com>
2016-05-21 03:21:07 +0800

    fixed

--- functions/posts/handler.js
+++ functions/posts/handler.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var DOC = require('dynamodb-doc');
-var dynamo = new DOC.DynamoDB();
+const DOC = require('dynamodb-doc');
+const dynamo = new DOC.DynamoDB();
 
 console.log('Loading function');
 
@@ -9,19 +9,21 @@ function display(object) {
     return JSON.stringify(object, null, 2)
 }
 
-module.exports.handler = function(event, context) {
+module.exports.handler = (event, context) => {
     console.log('Event: ', display(event));
     console.log('Context: ', display(context));
-    var operation = event.operation;
+    const operation = event.operation;
     if (event.tableName) {
         event.payload.TableName = event.tableName;
     }
     switch (operation) {
         case 'create':
-            var uuid = require('node-uuid');
+            const uuid = require('node-uuid');
             event.payload.Item.id = uuid.v1();
             console.log('Payload: ', display(event.payload));
-            dynamo.putItem(event.payload, context.succeed(event.payload.Item));
+            dynamo.putItem(event.payload, () => {
+                context.succeed(event.payload.Item);
+            });
             break;
         default:
             context.fail(new Error('Unrecognized operation "' + operation + '"'));
