41f1c4dc5d0d678b1695d6ca63a47e9b99033505
abalone0204 <abalone0204@gmail.com>
2016-05-21 23:57:13 +0800

    Done READ

---
+++ README.md
@@ -0,0 +1,27 @@
+# Serverless Demo: A simple RESTful API 
+
+# How to run?
+
+- You should install `serverless-framework` first
+
+```
+npm install -g serverless
+```
+
+- [And set up your aws](http://docs.serverless.com/docs/introducing-serverless)
+
+- Then clone this git repo to your project directory and run 
+
+```
+npm install
+```
+
+- Deploy
+
+```
+serverless dash deploy
+```
+
+- Then you have your own RESTful api.
+
+
--- functions/posts/handler.js
+++ functions/posts/handler.js
@@ -1,31 +1,34 @@
-'use strict';
+'use strict'
 
-const DOC = require('dynamodb-doc');
-const dynamo = new DOC.DynamoDB();
+const DOC = require('dynamodb-doc')
+const dynamo = new DOC.DynamoDB()
 
-console.log('Loading function');
+console.log('Loading function')
 
 function display(object) {
     return JSON.stringify(object, null, 2)
 }
 
 module.exports.handler = (event, context) => {
-    console.log('Event: ', display(event));
-    console.log('Context: ', display(context));
-    const operation = event.operation;
+    console.log('Event: ', display(event))
+    console.log('Context: ', display(context))
+    const operation = event.operation
     if (event.tableName) {
-        event.payload.TableName = event.tableName;
+        event.payload.TableName = event.tableName
     }
     switch (operation) {
         case 'create':
-            const uuid = require('node-uuid');
-            event.payload.Item.id = uuid.v1();
-            console.log('Payload: ', display(event.payload));
+            const uuid = require('node-uuid')
+            event.payload.Item.id = uuid.v1()
+            console.log('Payload: ', display(event.payload))
             dynamo.putItem(event.payload, () => {
-                context.succeed(event.payload.Item);
-            });
-            break;
+                context.succeed(event.payload.Item)
+            })
+            break
+        case 'read':
+            dynamo.getItem(event.payload, context.done)
+            break
         default:
-            context.fail(new Error('Unrecognized operation "' + operation + '"'));
+            context.fail(new Error('Unrecognized operation "' + operation + '"'))
     }
-};
+}
